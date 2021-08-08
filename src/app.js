4f93918218d5fe67a4589fbdbc60521f2501ada2
Tharatorn Supasiti <tharatornsupasiti@Tharatorns-MacBook-Air.local>
2021-08-08 22:43:04 +1000

    setting up app.js

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+// app.use('/api', api);
+app.use(express.static('public'));
+
+
+
+// GET Route for Default ----- PUT THIS LAST!
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+module.exports = app;
