a2adb13ee5f172a771f3cb34f6c5f3986e185bad
Thara Supasiti <sapiensis@gmail.com>
2021-08-13 16:17:35 +1000

    setting up sequelize

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+const sequelize = require('./src/configs/sequelizeConnection');
 const app = require('./src/app');
 const http = require('http');
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3001;
 
-server.listen(PORT, () => {
-  console.log(`listening on localhost:${PORT}`);
-});
+sequelize.sync({ force: false })
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`listening on localhost:${PORT}`);
+    });
+  });
