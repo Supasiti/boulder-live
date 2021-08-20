1895d32173d683accd5f980ef252288ec02967e1
Thara Supasiti <sapiensis@gmail.com>
2021-08-20 13:06:58 +1000

    add update problem

--- src/services/problemServices.js
+++ src/services/problemServices.js
@@ -11,23 +11,31 @@ const createProblemAssignments = async (problemId, categoryIds) => {
 };
 
 
-// create a new problems
+// create a new problem
 // arguments : name, categoryIds
 // return 
-//  - Event
+//  - Problem 
 const createOne = async (newProblem) => {
 
   const problemData = await models.Problem.create(newProblem);
   
   if ( 'categoryIds' in newProblem && newProblem.categoryIds.length) {
     const newProblemAssignments = await createProblemAssignments(problemData.id, newProblem.categoryIds);
-    return newProblemAssignments;
-  } else {
-    return problemData;
   }
+  return problemData
 }
 
-// remove an event from id
+// create new problems
+// arguments : name, 
+// return 
+//  - Problem 
+const createMany = async (newProblems) => {
+
+  const problemsData = await models.Problem.bulkCreate(newProblems);
+  return problemsData
+}
+
+// remove a problem from id
 // return 
 //  - int
 const remove = async (problemId) => {
@@ -37,22 +45,72 @@ const remove = async (problemId) => {
   return problemsRemoved;
 }
 
-// update an event 
-// return 
-//  - Event
-const update = async (newEvent, eventId) => {
-  const eventsUpdated = await Event.update(newEvent, {
-    where: { id: eventId }
+//----------------------------------------------------------------------------------------
+
+const getAssociatedProblemAssignments = async (problemId) => {
+  const assignments = await models.ProblemAssignment.findAll({
+    where : { problemId }
   })
-  if (!eventsUpdated[0]) return null;
-  const updatedEvent = await getEvents.byIds(eventId);
-  return updatedEvent[0];
+  
+  return assignments;
+}
+
+const getProblemAssignmentsToCreate = (oldCategoryIds, newCategoryIds, problemId) => {
+  const idsToCreate = newCategoryIds.filter((id) => !oldCategoryIds.includes(id))
+  const result = idsToCreate.map( (id) => {
+    return {
+      problemId,
+      categoryId: id
+    }
+  })
+  return result;
+}
+
+const getProblemAssignmentIdsToRemove = (oldAssignments, newCategoryIds) => {
+  const result = oldAssignments
+    .filter(({ categoryId }) => !newCategoryIds.includes(categoryId))
+    .map(({ id }) => id)
+  return result;
+}
+
+// update all assciated problem assignments 
+const updateAssociatedProblemAssignments = async (problemId, newCategoryIds) => {
+  const oldAssignments = await getAssociatedProblemAssignments(problemId);
+  const oldCategoryIds = oldAssignments.map(({ categoryId }) => categoryId);
+  const assignmentsToCreate = getProblemAssignmentsToCreate(oldCategoryIds, newCategoryIds, problemId);
+  const assignmentIdsToRemove = getProblemAssignmentIdsToRemove(oldAssignments, newCategoryIds);
+
+  const assignmentsRomoved = await models.ProblemAssignment.destroy({ where : { id : assignmentIdsToRemove }})
+  const newAssignments = await models.ProblemAssignment.bulkCreate(assignmentsToCreate);
+
+  return newAssignments;
+}
+
+
+
+// update a problem 
+// expect Object with problemId and new values
+// return 
+//  - Problem
+const update = async (newProblem) => {
+  const { problemId } = newProblem;
+  
+  const problemsUpdated = await models.Problem.update(newProblem, {
+    where: { id: problemId }
+  })
+  if ('categoryIds' in newProblem) {
+    await updateAssociatedProblemAssignments(problemId, newProblem.categoryIds)
+  }
+  const updatedProblem = models.Problem.findByPk(problemId);
+  return updatedProblem;
+
 }
 
 
 
 module.exports = {
   createOne,
+  createMany,
   update,
   remove
 }
--- src/tests/problemServices.test.js
+++ src/tests/problemServices.test.js
@@ -1,4 +1,5 @@
 const problem = require('../services/problemServices');
+const getProblems = require('../queries/getProblems');
 const sanitize = require('../services/sanitize')
 const models = require('../models');
 
@@ -12,16 +13,55 @@ describe('src/services/problemServices', () => {
     }
 
     const createdData = await problem.createOne(input);
-    const createdId = (createdData instanceof Array)? createdData[0].problemId : createdData.id;
-    const createdProblem = await models.Problem.findByPk(createdId);
-    await problem.remove(createdId);
+    await problem.remove(createdData.id);
     
-    expect(createdProblem.name).toEqual(input.name);
+    expect(createdData.name).toEqual(input.name);
+  })
 
-    
+  
+  // create and remove
+  it ('should create problems', async () => {
+    const input = [
+      {
+        name: "M3",
+        categoryIds : [1, 2]
+      },
+      {
+        name: "M4",
+        categoryIds : [1, 2]
+      }
+    ]
+
+    const createdData = await problem.createMany(input);
+    const createdIds = createdData.map(({ id }) => id); 
+    await Promise.all(
+      createdIds.map( (id) => { return problem.remove(id) })
+    )
+
+    expect(createdData.map(({ name }) => name))
+      .toEqual(expect.arrayContaining(input.map(({ name }) => name)));
   })
 
 
+
+  // update
+  it ('should update problem', async () => {
+    const firstInput = {
+      name: "M3",
+      categoryIds : [1, 2]
+    }
+    const { id } = await problem.createOne(firstInput);
+    const secondInput = {
+      problemId: id,
+      name: "M4",
+      categoryIds : [2, 3]
+    }
+    const updatedProblem = await problem.update(secondInput);
+    console.log(sanitize(updatedProblem));
+    await problem.remove(updatedProblem.id)
+  
+    expect(updatedProblem.name).toEqual(secondInput.name);
+  })
   
 
 })
