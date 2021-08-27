ce368d692542c2907111b45042ebcf5c28b575c3
Thara Supasiti <sapiensis@gmail.com>
2021-08-27 15:28:16 +1000

    add go to edit event page

--- public/js/goToEditEvent.js
+++ public/js/goToEditEvent.js
@@ -1,4 +1,25 @@
+const eventTables = document.querySelectorAll('.table');
 
-const got
+
+const cancelEvent = (event) => {
+  console.log('cancelling event ...')
+  event.stopImmediatePropagation();
+}
+
+const goToEditEventPage = (event) => {
+  event.preventDefault();
+
+  if (event.target.nodeName === 'BUTTON'){
+    cancelEvent(event);
+    return
+  }
+
+  // get event id...
+  const tableRow = event.target.closest('tr');
+  const eventId = tableRow.dataset.eventid;
+  console.log(eventId); 
+  document.location.replace(`/events/${eventId}`);
+}
 
 
+eventTables.forEach((table) => table.addEventListener('click', goToEditEventPage));
