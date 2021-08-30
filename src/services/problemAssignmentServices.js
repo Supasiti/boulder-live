696c7f60e50ad463346baad953fbbd2de0d0d9b0
Thara Supasiti <sapiensis@gmail.com>
2021-08-30 10:17:49 +1000

    delete redundant code in problemServices

--- src/services/problemAssignmentServices.js
+++ src/services/problemAssignmentServices.js
@@ -2,6 +2,8 @@ const models = require('../models');
 const query = require('../queries');
 
 //----------------------------------------------------------------------------------------
+
+// check if assignment is in an array 
 const isIn = (assignment, assignmentArray) => {
   const entry = assignmentArray.find((a) => {
     return a.problemId === assignment.problemId && a.categoryId === assignment.categoryId;
@@ -15,7 +17,6 @@ const getIdsToRemove = (oldAssignments, newAssignments) => {
     return !isIn(o, newAssignments);
   });
   const result = assignmentToRemove.map(({ id }) => id);
-  console.log('\nassignment services: ', result);
   return result;
 }
 
@@ -32,7 +33,6 @@ const getDataToCreate = (oldAssignments, newAssignments) => {
     // want to create the one not in the old set
     return !isIn(n, oldAssignments);
   });
-  console.log('\nassignment services: ', result);
   return result;
 }
 
--- src/services/problemServices.js
+++ src/services/problemServices.js
@@ -53,61 +53,15 @@ const remove = async (problemId) => {
 //----------------------------------------------------------------------------------------
 // UPDATE
 
-const getAssociatedProblemAssignments = async (problemId) => {
-  const assignments = await models.ProblemAssignment.findAll({
-    where : { problemId }
-  })
-  
-  return assignments;
-}
-
-const getProblemAssignmentsToCreate = (oldCategoryIds, newCategoryIds, problemId) => {
-  const idsToCreate = newCategoryIds.filter((id) => !oldCategoryIds.includes(id))
-  const result = idsToCreate.map( (id) => {
-    return {
-      problemId,
-      categoryId: id
-    }
-  })
-  return result;
-}
-
-const getProblemAssignmentIdsToRemove = (oldAssignments, newCategoryIds) => {
-  const result = oldAssignments
-    .filter(({ categoryId }) => !newCategoryIds.includes(categoryId))
-    .map(({ id }) => id)
-  return result;
-}
-
-// update all assciated problem assignments 
-const updateAssociatedProblemAssignments = async (problemId, newCategoryIds) => {
-  const oldAssignments = await getAssociatedProblemAssignments(problemId);
-  const oldCategoryIds = oldAssignments.map(({ categoryId }) => categoryId);
-  const assignmentsToCreate = getProblemAssignmentsToCreate(oldCategoryIds, newCategoryIds, problemId);
-  const assignmentIdsToRemove = getProblemAssignmentIdsToRemove(oldAssignments, newCategoryIds);
-
-  const assignmentsRomoved = await models.ProblemAssignment.destroy({ where : { id : assignmentIdsToRemove }})
-  const newAssignments = await models.ProblemAssignment.bulkCreate(assignmentsToCreate);
-
-  return newAssignments;
-}
-
 // update a problem 
 // expect Object with problemId and new values
 // return 
 //  - Problem
 const update = async (newProblem) => {
   const { problemId } = newProblem;
-  
-  const problemsUpdated = await models.Problem.update(newProblem, {
-    where: { id: problemId }
-  })
-  if ('categoryIds' in newProblem) {
-    await updateAssociatedProblemAssignments(problemId, newProblem.categoryIds)
-  }
+  await models.Problem.update(newProblem, { where: { id: problemId } })
   const updatedProblem = models.Problem.findByPk(problemId);
   return updatedProblem;
-
 }
 
 module.exports = {
